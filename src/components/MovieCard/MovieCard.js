c4ee4c804e42863934dde9bd8452670f10a6ae66
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-14 11:56:05 -0600

    Refactor buttons and sizings

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,7 +5,6 @@ import './MovieCard.css';
 export default function MovieCard ({movie}) {
     return (
         <div id={movie.id} className="card" style={{backgroundImage: `url(${movie.poster_path})`}}>
-            <img className="poster" src={movie.poster_path} style={{visibility: 'hidden'}} />
             <header className="card_header">{movie.title}</header>
             <footer className="card_footer">
                 <p className="footer_text">{movie.average_rating.toFixed(1)}</p>
