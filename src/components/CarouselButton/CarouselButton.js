4144c33b6eba9169fc05d712f5c35a0f3db2cf6d
Chris Anderson <mistercanderson@gmail.com>
2021-05-14 12:26:15 -0600

    Refactor component

--- src/components/CarouselButton/CarouselButton.js
+++ src/components/CarouselButton/CarouselButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './CarouselButton.css'
 
-export default function CarouselButton({ direction, updateDisplayStart}) {
+export default function CarouselButton({ direction, updateDisplayStart }) {
     return (
-        <div className="carousel_button">
-            {direction === "right" && <a className="arrow right" onClick={() => updateDisplayStart(direction)}></a>}
-            {direction === "left" && <a className="arrow left" onClick={() => updateDisplayStart(direction)}></a>}
-        </div>
+        <button
+            className={
+                `carousel_button 
+                ${direction === 'right' ? 'arrow-right ' : 'arrow-left '}
+                `}
+            onClick={() => updateDisplayStart(direction)}>
+        </button>
     )
 }
