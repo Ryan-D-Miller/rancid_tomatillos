258abde161087ef7d373f43827da374ee3d2d192
Chris Anderson <mistercanderson@gmail.com>
2021-05-13 19:30:54 -0600

    Improve styling for focus

--- src/components/MovieFocus/MovieFocus.js
+++ src/components/MovieFocus/MovieFocus.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './MovieFocus.css';
 // import './MovieCard.css'
 
-export default function MovieFocus({movie, focusClose}) {
+export default function MovieFocus({ movie, focusClose }) {
   return (
     <div id={movie.id} className="focus">
+      <header className="focus-header">
       <button onClick={focusClose} className='focus-close'>❌</button>
-      <img className="backdrop" src={movie.backdrop_path} />
-      <header className="focus_header">{movie.title}</header>
-      <footer className="focus_footer">
-        <p className="footer_text">{movie.average_rating.toFixed(1)}</p>
-        <p className="footer_text">{movie.release_date}</p>
+        <h1>{movie.title}</h1>
+      </header>
+      <img src={movie.backdrop_path} alt={movie.title}/>
+      <footer className="focus-footer">
+        <p className="footer-text">Rating: {movie.average_rating.toFixed(1)}</p>
+        <p className="footer-text">Release Date: {movie.release_date}</p>
       </footer>
     </div>
   )
