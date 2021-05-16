9e797bedc2f4ea31adf76d2976716b39f673fec9
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-16 13:14:38 -0600

    Change movieCarousel to a function instead of class

--- src/components/MovieCarousel/MovieCarousel.js
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -3,25 +3,21 @@ import MovieCard from '../MovieCard/MovieCard';
 import './MovieCarousel.css'
 import CarouselButton from '../CarouselButton/CarouselButton';
 
-class MovieCarousel extends Component {
-  constructor() {
-    super();
-    this.state = {
-      displayStart: 0,
-      displayRange: 5
-    }
-  }
-
-  render() {
+// class MovieCarousel extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       displayStart: 0,
+//       displayRange: 5
+//     }
+//   }
+export default function MovieCarousel({displayMovies, updateDisplayStart}) {
     
     return (
       <section className="carousel">
-        <CarouselButton direction={"left"} updateDisplayStart={this.props.updateDisplayStart}/>
-        {this.props.displayMovies()}
-        <CarouselButton direction={"right"} updateDisplayStart={this.props.updateDisplayStart}/>
+        <CarouselButton direction={"left"} updateDisplayStart={updateDisplayStart}/>
+        {displayMovies()}
+        <CarouselButton direction={"right"} updateDisplayStart={updateDisplayStart}/>
       </section>
     )
-  }
 }
-
-export default MovieCarousel
