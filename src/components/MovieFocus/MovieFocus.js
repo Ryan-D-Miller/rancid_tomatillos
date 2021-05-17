712f425eee2ff36b3ae56007239d4f80630e9e79
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-17 17:59:39 -0600

    Add more information to movie focus

--- src/components/MovieFocus/MovieFocus.js
+++ src/components/MovieFocus/MovieFocus.js
@@ -6,12 +6,29 @@ export default function MovieFocus({ movie, focusClose }) {
     <div id={movie.id} className="focus">
       <header className="focus-header">
       <button onClick={focusClose}>x</button>
-        <h1>{movie.title}</h1>
+        <div className="header_title">
+            <h1>{movie.title}</h1>
+            <p>{movie.tagline}</p>
+            <p>Release Date: {movie.release_date}</p>
+        </div>
       </header>
       <img src={movie.backdrop_path} alt={movie.title}/>
-      <footer className="focus-footer">
-        <p className="footer-text">Rating: {movie.average_rating.toFixed(1)}</p>
-        <p className="footer-text">Release Date: {movie.release_date}</p>
+      <section className="overview">{movie.overview}</section>
+      <footer >
+        <div className="focus-footer">
+          <p className="footer-text">Rating: {movie.average_rating.toFixed(1)}</p>
+          <p className="footer-text">Revenue: ${movie.revenue.toLocaleString('en-US')}</p>
+          <p className="footer-text">Budget: ${movie.budget.toLocaleString('en-US')}</p>
+          <p className="footer-text">Run Time: {movie.runtime} minutes</p>
+        </div>
+        <div className="focus-footer">
+          <p>Genres</p>
+        </div>
+        <div className="focus-footer">
+          {movie.genres.map(genre => {
+            return <p>{genre}</p>
+          })}
+        </div>
       </footer>
     </div>
   )
