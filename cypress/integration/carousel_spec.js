f191ef2adf99f3dc2f15a5cbe391e8092b096707
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-17 16:17:13 -0600

    Add test for checking if the input fields exist

--- cypress/integration/carousel_spec.js
+++ cypress/integration/carousel_spec.js
@@ -3,7 +3,30 @@ describe('Carsouel', () => {
         cy.loadStubs();
         cy.visit('/')
     });
-    it('should return true', () => {
-        expect(true).to.equal(true);
-    })
+
+    it('should display five movie cards', () => {
+        cy.get('.carousel')
+            .children('.card')
+            .should('have.length', 5)
+    });
+
+    it('should have two buttons for navigation', () => {
+        cy.get('.carousel')
+            .children('.carousel-button-wrapper')
+            .should('have.length', 2) 
+    });
+
+    it('should display 5 new movies when a carousel button is pressed', () => {
+        cy.get('.carousel')
+            .find('.card').first().should('have.id', '694919');
+        cy.get('.arrow-right').click();
+        cy.get('.carousel')
+            .find('.card').first().should('have.id', '726739');
+    });
+
+    it('should be able to display a single movie when clicking on a movie', () => {
+        cy.get('.carousel')
+          .find('.card').first().click()
+        cy.get('h1').contains('Money Plane')
+    });
 });
--- cypress/integration/header_spec.js
+++ cypress/integration/header_spec.js
@@ -1,8 +1,19 @@
 describe('Header', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/')
+        cy.loadStubs();
+        cy.visit('/')
     })
     it('Should visit the page and render the header', () => {
-        cy.get('img[alt="Rancid Tomatillos Logo"]')
+        cy.get('.logo').should('have.attr', 'src')
+            .should('include', 'rancid_tomatillos_logo')
+    });
+    it('should have a filter and search input fields', () => {
+        // cy.get('.header').get('form').get('input[type="search"]')
+        cy.get('.header').within(() => {
+            cy.get('input[type="search"]')
+        });
+        cy.get('.header').within(() => {
+            cy.get('select')
+        })
     });
 });
