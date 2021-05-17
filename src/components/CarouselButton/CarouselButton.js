405918f5a66fd2d0b81986e2bcd6e86c9d82ad14
Chris Anderson <mistercanderson@gmail.com>
2021-05-16 18:23:09 -0600

    Style CarouselButton

--- src/components/CarouselButton/CarouselButton.js
+++ src/components/CarouselButton/CarouselButton.js
@@ -3,12 +3,14 @@ import './CarouselButton.css'
 
 export default function CarouselButton({ direction, updateDisplayStart }) {
     return (
-        <button
-            className={
-                `carousel_button 
-                ${direction === 'right' ? 'arrow-right ' : 'arrow-left '}
-                `}
-            onClick={() => updateDisplayStart(direction)}>
-        </button>
+        <div className='carousel-button-wrapper'>
+            <button
+                className={
+                    `carousel-button 
+                    ${direction === 'right' ? 'arrow-right ' : 'arrow-left '}
+                    `}
+                onClick={() => updateDisplayStart(direction)}>
+            </button>
+        </div>
     )
 }
