c8c9bcd795f9d5a3075450abe8c97109367f0a8c
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-18 13:13:59 -0600

    Add test to see if back button works

--- cypress/integration/movieFocus_spec.js
+++ cypress/integration/movieFocus_spec.js
@@ -8,4 +8,10 @@ describe('Movie Focus', () => {
     it('Should display the movie title', () => {
         cy.get('h1').contains('Money Plane');
     });
+    it('should be able to click the x button to go back to the main movie view', () => {
+        cy.get('button').click()
+        cy.get('.carousel')
+            .children('.card')
+            .should('have.length', 5)
+    });
 })
