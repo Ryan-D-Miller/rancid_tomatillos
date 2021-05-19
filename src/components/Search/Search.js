03738bff6516c242a0c5c19335961865bc0f503c
Chris Anderson <mistercanderson@gmail.com>
2021-05-19 10:09:06 -0600

    Refactor search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Search extends Component {
-  constructor() {
-    super();
-    // this.state = {
-    //   query: ''
-    // }
-  }
-
-  render() {
-    return (
-      <input 
-        type='search'
-        onChange={this.props.searchMovies}
-        placeholder='Search Movies...'>
-
-      </input>
-    )
-  }
+export default function Search(props) {
+  return (
+    <input
+      type='search'
+      onChange={props.searchMovies}
+      placeholder='Search Movies...'>
+    </input>
+  )
 }
