618fea0a12c3e3c09d1effeaa29cf21a84d51666
Chris Anderson <mistercanderson@gmail.com>
2021-05-21 12:41:29 -0600

    Style hamburger

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -7,8 +7,10 @@ import './Hamburger.css';
 export default function Hamburger({ movies, searchMovies, filterMovies }) {
   return (
     <Menu>
-      <Search searchMovies={searchMovies} />
-      <Filter movies={movies} filterMovies={filterMovies} />
+      <div>
+        <Search searchMovies={searchMovies} />
+        <Filter movies={movies} filterMovies={filterMovies} />
+      </div>
     </Menu>
   );
 }
