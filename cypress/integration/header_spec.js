ff99d59df4a63884953ce198f0c67d1e80572c1b
Chris Anderson <mistercanderson@gmail.com>
2021-05-22 11:55:08 -0600

    Add testing for Header

--- cypress/integration/header_spec.js
+++ cypress/integration/header_spec.js
@@ -1,19 +1,42 @@
 describe('Header', () => {
-    beforeEach(() => {
-        cy.loadStubs();
-        cy.visit('/')
-    })
-    it('Should visit the page and render the header', () => {
-        cy.get('.logo').should('have.attr', 'src')
-            .should('include', 'rancid_tomatillos_logo')
-    });
-    it('should have a filter and search input fields', () => {
-        // cy.get('.header').get('form').get('input[type="search"]')
-        cy.get('.header').within(() => {
-            cy.get('input[type="search"]')
-        });
-        cy.get('.header').within(() => {
-            cy.get('select')
-        })
-    });
+  beforeEach(() => {
+    cy.loadStubs();
+    cy.visit('/');
+    cy.get('header')
+  });
+
+  it('Should have a logo', () => {
+    cy.get('.logo');
+  });
+
+  it('Should return to the main url when clicking the logo', () => {
+    cy.get('.carousel')
+    .get('.card').first()
+    .click();
+    cy.url().should('eq', 'http://localhost:3000/694919')
+    cy.get('.logo').first()
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+
+  it('should have filter and search input fields', () => {
+      cy.get('input[type="search"]');
+      cy.get('select');
+  });
+
+  it('Should be able to filter Alphabetically', () => {
+    cy.get('select')
+    .select('Alphabetically')
+    // test that first card is the right movie
+  });
+
+  it('Should be able to filter by Release Date', () => {
+    cy.get('select')
+    .select('Release Date')
+  });
+
+  it('Should be able to filter by Rating', () => {
+    cy.get('select')
+    .select('Rating')
+  });
 });
