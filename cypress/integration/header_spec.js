485c535646c20f0c7fe7b7b0e61a0a4ef1b64a2b
Chris Anderson <mistercanderson@gmail.com>
2021-05-22 15:44:36 -0600

    Add tests for search/filter

--- cypress/integration/header_spec.js
+++ cypress/integration/header_spec.js
@@ -2,7 +2,7 @@ describe('Header', () => {
   beforeEach(() => {
     cy.loadStubs();
     cy.visit('/');
-    cy.get('header')
+    cy.get('header');
   });
 
   it('Should have a logo', () => {
@@ -10,33 +10,29 @@ describe('Header', () => {
   });
 
   it('Should return to the main url when clicking the logo', () => {
-    cy.get('.carousel')
-    .get('.card').first()
-    .click();
-    cy.url().should('eq', 'http://localhost:3000/694919')
-    cy.get('.logo').first()
-    .click()
-    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('.carousel').get('.card').first().click();
+    cy.url().should('eq', 'http://localhost:3000/694919');
+    cy.get('.logo').first().click();
+    cy.url().should('eq', 'http://localhost:3000/');
   });
 
   it('should have filter and search input fields', () => {
-      cy.get('input[type="search"]');
-      cy.get('select');
+    cy.get('input[type="search"]');
+    cy.get('select');
   });
 
   it('Should be able to filter Alphabetically', () => {
-    cy.get('select')
-    .select('Alphabetically')
-    // test that first card is the right movie
-  });
+    cy.get('select').select('Alphabetically');
+    cy.get('.card').first().children().first().should('have.id', 528085)
+  })
 
   it('Should be able to filter by Release Date', () => {
-    cy.get('select')
-    .select('Release Date')
+    cy.get('select').select('Release Date');
+    cy.get('.card').first().children().first().should('have.id', 726739)
   });
 
   it('Should be able to filter by Rating', () => {
-    cy.get('select')
-    .select('Rating')
+    cy.get('select').select('Rating');
+    cy.get('.card').first().children().first().should('have.id', 726739)
   });
 });
