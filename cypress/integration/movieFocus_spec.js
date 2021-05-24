d76003a50f0730ba4593aee2190924d200be636b
Ryan Miller <ryandmiller3521@gmail.com>
2021-05-24 15:18:23 -0600

    Re-add movie Focus

--- cypress/integration/movieFocus_spec.js
+++ cypress/integration/movieFocus_spec.js
@@ -5,13 +5,32 @@ describe('Movie Focus', () => {
         cy.get('.carousel')
             .find('.card').first().click()
     });
-    it('Should display the movie title', () => {
-        cy.get('h1').contains('Money Plane');
+
+    it('Should change the url path when clicking on a movie', () => {
+        cy.url().should('eq', 'http://localhost:3000/694919')
     });
+
+    it('Should display movie information', () => {
+        cy.get('.focus')
+            .contains('Money Plane');
+        cy.get('.focus')
+            .contains('Release Date');
+        cy.get('.focus')
+            .contains('Run Time');
+        cy.get('.focus')
+            .contains('Overview');
+    });
+
     it('should be able to click the x button to go back to the main movie view', () => {
         cy.get('button').click()
         cy.get('.carousel')
             .children('.card')
-            .should('have.length', 5)
+            .contains('Money Plane')
     });
+
+    it('Should change the url path when exiting Movie Focus', () => {
+        cy.get('button').click()
+        cy.url().should('eq', 'http://localhost:3000/')
+    });
+
 })
